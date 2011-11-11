d7e34145c8ddc64268880dde5ec8ce94bd33cb00
Jon Ludlam <jonathan.ludlam@eu.citrix.com>
2011-11-11 16:02:24 +0000

    Add GPLv2 licensing
    Signed-off-by: Jon Ludlam <jonathan.ludlam@eu.citrix.com>

--- control.c
+++ control.c
@@ -1,3 +1,25 @@
+/*
+ *
+ * Copyright (C) 2011 Citrix Systems Inc.
+ *
+ * This file is part of Blktap2.
+ *
+ * Blktap2 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * Blktap2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License version 2 for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with Blktap2.  If not, see 
+ * <http://www.gnu.org/licenses/>.
+ *
+ *
+ */
+
 #include <linux/module.h>
 #include <linux/sched.h>
 #include <linux/miscdevice.h>
